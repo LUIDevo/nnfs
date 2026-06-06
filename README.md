f08e1699233cc2d57bde52a6a4201d2f1b24e5a7
louis <sanicisspeedandfast@gmail.com>
2026-06-06 18:39:35 -0400

    readme and updated main.py

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository is a place where I can practice building a neural network from scratch using the NNFS book.
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import numpy as np
 
-inputs=[1,2,3,2.5]
-weights=[[0.2,0.8,-0.5,1], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]
-biases=[2,3,0.5]
-
-
-print(np.dot(weights, inputs)+biases)
+inputs = [[1.0, 2.0, 3.0, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, -0.8]]
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+biases = [2.0, 3.0, 0.5]
+layer_outputs = np.dot(inputs, np.array(weights).T) + biases
+# given a batch of 3 inputs (3x4), and 3x4 weights it creats an output using dot product given a transposition of the weights, and adds biases
+print(layer_outputs)
