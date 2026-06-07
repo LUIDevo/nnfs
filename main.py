65d82c948b61be8460271a92278ecbdfd82d883b
louis <sanicisspeedandfast@gmail.com>
2026-06-06 23:45:20 -0400

    updated

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import numpy as np
-
-inputs = [[1.0, 2.0, 3.0, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
+from nnfs.datasets import spiral_data
+inputs = [[1, 2, 3, 2.5],
+          [2., 5., -1., 2],
           [-1.5, 2.7, 3.3, -0.8]]
-weights = [[0.2, 0.8, -0.5, 1.0],
+weights = [[0.2, 0.8, -0.5, 1],
            [0.5, -0.91, 0.26, -0.5],
            [-0.26, -0.27, 0.17, 0.87]]
-biases = [2.0, 3.0, 0.5]
-layer_outputs = np.dot(inputs, np.array(weights).T) + biases
-# given a batch of 3 inputs (3x4), and 3x4 weights it creats an output using dot product given a transposition of the weights, and adds biases
-print(layer_outputs)
+biases = [2, 3, 0.5]
+weights2 = [[0.1, -0.14, 0.5],
+            [-0.5, 0.12, -0.33],
+            [-0.44, 0.73, -0.13]]
+biases2 = [-1, 2, -0.5]
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+# visually, this is 4 inputs, into 2 layers of 3 neurons
+print(layer2_outputs)
