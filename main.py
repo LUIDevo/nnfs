e6a8f4b2f216017bdae5060e9bc63643b4c962f1
louis <sanicisspeedandfast@gmail.com>
2026-06-08 01:56:35 -0400

    relu, softmax

--- main.py
+++ main.py
@@ -15,7 +15,43 @@ class Layer_Dense:
 x, y = spiral_data(samples=100, classes=3)
 dense1=Layer_Dense(2,3)
 # creating the layer with 3 neurons, 2 inputs
+class Activation_ReLU:
+    # Forward pass
+    def forward(self, inputs):
+        # Calculate output values from inputs
+        self.output = np.maximum(0, inputs)
+# Softmax activation
+class Activation_Softmax:
+    # Forward pass
+    def forward(self, inputs):
+        # Get unnormalized probabilities
+        exp_values = np.exp(inputs - np.max(inputs, axis=1,
+                                            keepdims=True))
+        # Normalize them for each sample
+        probabilities = exp_values / np.sum(exp_values, axis=1,
+                                            keepdims=True)
+        self.output = probabilities
+# Create dataset
+X, y = spiral_data(samples=100, classes=3)
+# Create Dense layer with 2 input features and 3 output values
+dense1 = Layer_Dense(2, 3)
+# Create ReLU activation (to be used with Dense layer):
+activation1 = Activation_ReLU()
+# Create second Dense layer with 3 input features (as we take output
+# of previous layer here) and 3 output values
+dense2 = Layer_Dense(3, 3)
+# Create Softmax activation (to be used with Dense layer):
+activation2 = Activation_Softmax()
+# Make a forward pass of our training data through this layer
+dense1.forward(X)
+# Make a forward pass through activation function
+# it takes the output of first dense layer here
+activation1.forward(dense1.output)
+# Make a forward pass through second Dense layer
+# it takes outputs of activation function of first layer as inputs
+dense2.forward(activation1.output)
+# Make a forward pass through activation function
+# it takes the output of second dense layer here
+activation2.forward(dense2.output)
 
-dense1.forward(x)
-
-print(dense1.output[:5])
+print(activation2.output[:5])
