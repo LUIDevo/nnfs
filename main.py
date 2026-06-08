c2c093c20be05586f7d391d3a9bd56dbb48999e0
louis <sanicisspeedandfast@gmail.com>
2026-06-07 21:54:35 -0400

    using classes to create layers

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import numpy as np
+import nnfs
 from nnfs.datasets import spiral_data
-inputs = [[1, 2, 3, 2.5],
-          [2., 5., -1., 2],
-          [-1.5, 2.7, 3.3, -0.8]]
-weights = [[0.2, 0.8, -0.5, 1],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-biases = [2, 3, 0.5]
-weights2 = [[0.1, -0.14, 0.5],
-            [-0.5, 0.12, -0.33],
-            [-0.44, 0.73, -0.13]]
-biases2 = [-1, 2, -0.5]
-layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
-# visually, this is 4 inputs, into 2 layers of 3 neurons
-print(layer2_outputs)
+nnfs.init()
+# Dense layer
+class Layer_Dense:
+    # Layer initialization
+    def __init__(self, n_inputs, n_neurons):
+        # Initialize weights and biases
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output=np.dot(inputs, self.weights)+self.biases
+
+x, y = spiral_data(samples=100, classes=3)
+dense1=Layer_Dense(2,3)
+# creating the layer with 3 neurons, 2 inputs
+
+dense1.forward(x)
+
+print(dense1.output[:5])
