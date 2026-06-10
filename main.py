afa05932b1005452af93991377eea552c95d5c35
louis <sanicisspeedandfast@gmail.com>
2026-06-10 15:54:22 -0400

    added backprop

--- main.py
+++ main.py
@@ -8,11 +8,20 @@ class Layer_Dense:
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
     def forward(self, inputs):
+        self.inputs = inputs
         self.output = np.dot(inputs, self.weights) + self.biases
+    def backward(self, dvalues):
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        self.dinputs = np.dot(dvalues, self.weights.T)
 
 class Activation_ReLU:
     def forward(self, inputs):
+        self.inputs = inputs
         self.output = np.maximum(0, inputs)
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
 
 class Activation_Softmax:
     def forward(self, inputs):
@@ -31,51 +40,56 @@ class Loss_CategoricalCrossentropy(Loss):
         samples = len(y_pred)
         y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
         if len(y_true.shape) == 1:
-            correct_confidences = y_pred_clipped[
-                range(samples),
-                y_true
-            ]
+            correct_confidences = y_pred_clipped[range(samples), y_true]
         elif len(y_true.shape) == 2:
-            correct_confidences = np.sum(
-                y_pred_clipped*y_true,
-                axis=1
-            )
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
         negative_log_likelihoods = -np.log(correct_confidences)
         return negative_log_likelihoods
+
+class Activation_Softmax_Loss_CategoricalCrossentropy:
+    def __init__(self):
+        self.activation = Activation_Softmax()
+        self.loss = Loss_CategoricalCrossentropy()
+    def forward(self, inputs, y_true):
+        self.activation.forward(inputs)
+        self.output = self.activation.output
+        return self.loss.calculate(self.output, y_true)
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+        if len(y_true.shape) == 2:
+            y_true = np.argmax(y_true, axis=1)
+        self.dinputs = dvalues.copy()
+        self.dinputs[range(samples), y_true] -= 1
+        self.dinputs = self.dinputs / samples
+
 X, y = vertical_data(samples=100, classes=3)
 dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
 dense2 = Layer_Dense(3, 3)
-activation2 = Activation_Softmax()
-loss_function = Loss_CategoricalCrossentropy()
-lowest_loss = 99999999999
-best_dense1_weights = dense1.weights.copy()
-best_dense1_biases = dense1.biases.copy()
-best_dense2_weights = dense2.weights.copy()
-best_dense2_biases = dense2.biases.copy()
+loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
+lr = 1
 
 for iteration in range(10000):
-    dense1.weights+=0.05 * np.random.randn(2,3)
-    dense1.biases+=0.05 * np.random.randn(1,3)
-    dense2.weights+=0.05 * np.random.randn(3,3)
-    dense2.biases+=0.05 * np.random.randn(1,3)
+    # forward
     dense1.forward(X)
     activation1.forward(dense1.output)
     dense2.forward(activation1.output)
-    activation2.forward(dense2.output)
-    loss=loss_function.calculate(activation2.output, y)
-    predictions = np.argmax(activation2.output, axis=1)
+    loss = loss_activation.forward(dense2.output, y)
+
+    predictions = np.argmax(loss_activation.output, axis=1)
     accuracy = np.mean(predictions == y)
 
-    if loss < lowest_loss:
-        print('New set of weights found, iteration: ', iteration, 'loss:', loss, 'acc:', accuracy)
-        best_dense1_weights = dense1.weights.copy()
-        best_dense1_biases = dense1.biases.copy()
-        best_dense2_weights = dense2.weights.copy()
-        best_dense2_biases = dense2.biases.copy()
-        lowest_loss = loss
-    else:
-        dense1.weights = best_dense1_weights.copy()
-        dense1.biases = best_dense1_biases.copy()
-        dense2.weights = best_dense2_weights.copy()
-        dense2.biases = best_dense2_biases.copy()
+    if not iteration % 1000:
+        print(f'iteration: {iteration}, loss: {loss:.4f}, acc: {accuracy:.3f}')
+
+    # backward
+    loss_activation.backward(loss_activation.output, y)
+    dense2.backward(loss_activation.dinputs)
+    activation1.backward(dense2.dinputs)
+    dense1.backward(activation1.dinputs)
+
+    # gradient descent update
+    dense1.weights += -lr * dense1.dweights
+    dense1.biases  += -lr * dense1.dbiases
+    dense2.weights += -lr * dense2.dweights
+    dense2.biases  += -lr * dense2.dbiases
