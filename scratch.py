0fb81885103ecaf289cf3024e2251f5aa103a3bc
louis <sanicisspeedandfast@gmail.com>
2026-06-21 20:16:46 -0400

    layer dense

--- scratch.py
+++ scratch.py
@@ -4,6 +4,17 @@ from nnfs.datasets import vertical_data
 
 nnfs.init()
 
+class LayerDense:
+    def __init__(self, feautres, neurons):
+        self.weights=0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases=np.zeros((1, neurons))
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+
 # program setup
 X, y = vertical_data(samples=100, classes=3)
+dense1=
 # program loop
